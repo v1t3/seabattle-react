9a494a0c1579d573603a80b42b5885756d1a84ca
Vitali <37334443+v1t3@users.noreply.github.com>
2019-10-03 23:30:31 +0300

    add Field

--- seabattle-app/src/components/App.js
+++ seabattle-app/src/components/App.js
@@ -1,4 +1,5 @@
 import React, {Component} from 'react'
+import Field from './Field'
 import './style.css'
 
 class App extends Component {
@@ -14,64 +15,12 @@ class App extends Component {
 
           <div className="bfield bfield1">
             <p className="btext btext1">Игрок 1: <span id="username1">Юзер</span></p>
-            <div className="field field-user">
-              <div className="top-nums">
-                <div className="top-num top-num1">1</div>
-                <div className="top-num top-num2">2</div>
-                <div className="top-num top-num3">3</div>
-                <div className="top-num top-num4">4</div>
-                <div className="top-num top-num5">5</div>
-                <div className="top-num top-num6">6</div>
-                <div className="top-num top-num7">7</div>
-                <div className="top-num top-num8">8</div>
-                <div className="top-num top-num9">9</div>
-                <div className="top-num top-num10">10</div>
-              </div>
-              <div className="left-nums">
-                <div className="left-num left-num1">А</div>
-                <div className="left-num left-num2">Б</div>
-                <div className="left-num left-num3">В</div>
-                <div className="left-num left-num4">Г</div>
-                <div className="left-num left-num5">Д</div>
-                <div className="left-num left-num6">Е</div>
-                <div className="left-num left-num7">Ж</div>
-                <div className="left-num left-num8">З</div>
-                <div className="left-num left-num9">И</div>
-                <div className="left-num left-num10">К</div>
-              </div>
-              <div id="field_user" className="ships"></div>
-            </div>
+            {Field}
           </div>
 
           <div className="bfield bfield2">
             <p className="btext btext2">Игрок 2: <span>Компьютер</span></p>
-            <div className="field field-comp">
-              <div className="top-nums">
-                <div className="top-num top-num1">1</div>
-                <div className="top-num top-num2">2</div>
-                <div className="top-num top-num3">3</div>
-                <div className="top-num top-num4">4</div>
-                <div className="top-num top-num5">5</div>
-                <div className="top-num top-num6">6</div>
-                <div className="top-num top-num7">7</div>
-                <div className="top-num top-num8">8</div>
-                <div className="top-num top-num9">9</div>
-                <div className="top-num top-num10">10</div>
-              </div>
-              <div className="left-nums">
-                <div className="left-num left-num1">А</div>
-                <div className="left-num left-num2">Б</div>
-                <div className="left-num left-num3">В</div>
-                <div className="left-num left-num4">Г</div>
-                <div className="left-num left-num5">Д</div>
-                <div className="left-num left-num6">Е</div>
-                <div className="left-num left-num7">Ж</div>
-                <div className="left-num left-num8">З</div>
-                <div className="left-num left-num9">И</div>
-                <div className="left-num left-num10">К</div>
-              </div>
-              <div id="field_comp" className="ships"></div>
-            </div>
+            {Field}
           </div>
 
         </div>
