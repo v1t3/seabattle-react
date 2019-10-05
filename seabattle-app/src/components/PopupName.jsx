37490bbd5cc2af5400f718644bca1a161cb8c974
Vitali <37334443+v1t3@users.noreply.github.com>
2019-10-06 00:21:05 +0300

    renamed

--- seabattle-app/src/components/PopupName.jsx
+++ seabattle-app/src/components/PopupName.jsx
@@ -1,6 +1,7 @@
 import React, {Component} from 'react'
 
-export default class PopupName extends Component {
+export default 
+class PopupName extends Component {
   constructor(props) {
     super(props);
     this.state = {
